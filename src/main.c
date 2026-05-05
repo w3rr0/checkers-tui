4b4ac90b777585f5087ed15ce0046a9e61538163
w3rr0 <konradmateja65@gmail.com>
2026-05-05 13:58:37 +0200

    init board in main

--- src/main.c
+++ src/main.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 
+#include "board.h"
+
 int main(void) {
-    printf("Hello, World!\n");
-    return 0;
+  Board board = {};
+  init_board(&board);
+  print_board(&board);
+
+  printf("Turn: %s\n", board.white_turn ? "WHITE" : "BLACK");
+  return 0;
 }
