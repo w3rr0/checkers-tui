39c3b3cdc8b6dd6a62c78d5ee72e95fedb0db290
w3rr0 <konradmateja65@gmail.com>
2026-05-06 10:00:45 +0200

    add get_user_move func

--- src/main.c
+++ src/main.c
@@ -2,6 +2,22 @@
 
 #include "board.h"
 
+Move get_user_move() {
+  char col_from, col_to;
+  int row_from, row_to;
+
+  printf("Enter your move: ");
+
+  while (true) {
+    if (scanf("%c%d %c%d", &col_from, &row_from, &col_to, &row_to) == 4) {
+      break;
+    }
+  }
+
+  return (Move){.from = {.x = col_from - 'A', .y = row_from - 1},
+                .to = {.x = col_to - 'A', .y = row_to - 1}};
+}
+
 int main(void) {
   Board board = {};
   init_board(&board);
