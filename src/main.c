9f48eea227ead4c9dd4eb51083c7d5c35d6d0f32
w3rr0 <konradmateja65@gmail.com>
2026-05-06 18:31:43 +0200

    change func order

--- src/main.c
+++ src/main.c
@@ -3,6 +3,27 @@
 
 #include "board.h"
 
+Move get_user_move();
+
+int main(void) {
+  Board board = {};
+  init_board(&board);
+
+  while (true) {
+    print_board(&board);
+    printf("Turn: %s\n", board.white_turn ? "WHITE" : "BLACK");
+
+    auto m = get_user_move();
+
+    if (!execute_move(&board, m)) {
+      printf("\033[31mWrong move! Please try again\033[0m\n");
+      continue;
+    }
+  }
+
+  return 0;
+}
+
 Move get_user_move() {
   char col_from, col_to;
   int row_from, row_to;
@@ -22,23 +43,4 @@ Move get_user_move() {
 
   return (Move){.from = {.x = toupper(col_from) - 'A', .y = row_from - 1},
                 .to = {.x = toupper(col_to) - 'A', .y = row_to - 1}};
-}
-
-int main(void) {
-  Board board = {};
-  init_board(&board);
-
-  while (true) {
-    print_board(&board);
-    printf("Turn: %s\n", board.white_turn ? "WHITE" : "BLACK");
-
-    auto m = get_user_move();
-
-    if (!execute_move(&board, m)) {
-      printf("\033[31mWrong move! Please try again\033[0m\n");
-      continue;
-    }
-  }
-
-  return 0;
 }
