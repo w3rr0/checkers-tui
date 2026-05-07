8fc6985b4e25cfc7c0794a9e465c5f223e802aeb
w3rr0 <konradmateja65@gmail.com>
2026-05-07 19:08:04 +0200

    end loop when someone win

--- src/main.c
+++ src/main.c
@@ -12,14 +12,23 @@ int main(void) {
   while (true) {
     print_board(&board);
 
+    bool winner = true;
+
     // Check for win
     if (board.white_pieces == 0) {
       printf("BLACK WINS!");
     } else if (board.black_pieces == 0) {
       printf("WHITE WINS!");
     } else {
-      printf("Turn: %s\n", board.white_turn ? "WHITE" : "BLACK");
+      winner = false;
     }
+
+    if (winner) {
+      printf("\n");
+      break;
+    }
+
+    printf("Turn: %s\n", board.white_turn ? "WHITE" : "BLACK");
 
     auto m = get_user_move();
 
